d2c9c248f4339e73a48fad1cbea12cd01e72a448
André Leão <94919094+andreleao81@users.noreply.github.com>
2024-06-18 23:15:52 -0300

    Fix

--- app/schemas/CurriculumSchema.py
+++ app/schemas/CurriculumSchema.py
@@ -10,14 +10,14 @@ class CurriculumSchema(ma.SQLAlchemySchema):
     
     id = ma.Integer(dump_only=True)
     class_code = ma.String(required=True)
-    conclusion_semester = ma.String(required=True)
+    conclusion_semester = ma.Integer(required=True)
     conclusion = ma.Boolean(required=True, default=False)
     user_id = ma.Integer(required=True)
         
-    @validates('conclusion_semester')
-    def validate_semester(self, value, **kwargs):
-        if not 1 <= int(value) <= 20:
-            raise ValidationError('Semester must be greater than 0')
+    # @validates('conclusion_semester')
+    # def validate_semester(self, value, **kwargs):
+    #     if not 1 <= int(value) <= 20:
+    #         raise ValidationError('Semester must be greater than 0')
     
     @post_load
     def make_upper(self, data, **kwargs):
